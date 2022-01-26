465a94da55cabc84deb0fb200d3b2325b72bbe0d
Nikolay Naumenkov <nikolas.it.web@gmail.com>
2022-01-26 18:49:35 +0300

    Fix tests

--- test/loader.test.js
+++ test/loader.test.js
@@ -1,18 +1,20 @@
 const compiler = require('./compiler.js');
 
-test('compile simple rule', async () => {
-  const stats = await compiler('simple.rule');
-  const output = stats.toJson({ source: true }).modules[0].source;
-  expect(output).toMatchSnapshot();
-});
+describe('rule-loader', () => {
+  it('should output compiled rule module', async () => {
+    const stats = await compiler('simple.rule');
+    const output = stats.toJson({ source: true }).modules[0].source;
+    expect(output).toMatchSnapshot();
+  });
 
-test('bad schema rule', async () => {
-  expect.assertions(1);
-  let message = null;
-  try {
-    await compiler('bad-schema.rule');
-  } catch (e) {
-    message = e[0].message.split("\n")[1];
-  }
-  expect(message).toMatchSnapshot();
+  it('should throw error when invalid rule schema', async () => {
+    expect.assertions(1);
+    let message = null;
+    try {
+      await compiler('bad-schema.rule');
+    } catch (e) {
+      message = e[0].message.split("\n")[1];
+    }
+    expect(message).toMatchSnapshot();
+  });
 });
